f674693f29baa0f39c7997d14db9892a27f70486
IgorChernov <ichernomorr@gmail.com>
2023-08-26 12:36:31 +0300

    Add Hibernate annotations to entity Book

--- src/main/java/com/belhard/bookstore/data/entity/Book.java
+++ src/main/java/com/belhard/bookstore/data/entity/Book.java
@@ -2,19 +2,30 @@ package com.belhard.bookstore.data.entity;
 
 import lombok.Data;
 
+import jakarta.persistence.*;
 import java.math.BigDecimal;
 
 @Data
+@Entity
+@Table(name = "books")
 public class Book {
-
-    private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "title")
     private String title;
+    @Column(name = "author")
     private String author;
+    @Column(name = "year")
     private String year;
+    @Column(name = "price")
     private BigDecimal price;
+    @Column(name = "pages")
     private int pages;
+    @Column(name = "isbn")
     private String isbn;
-
+    @Column(name = "cover_type_id")
     private Cover cover;
 
     public enum Cover {
