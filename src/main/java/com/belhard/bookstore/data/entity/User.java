4b8516a48f34a635d07ccb2b8053d4e90ca3d951
IgorChernov <ichernomorr@gmail.com>
2023-08-26 13:34:42 +0300

    Add Hibernate annotations to entity User

--- src/main/java/com/belhard/bookstore/data/entity/User.java
+++ src/main/java/com/belhard/bookstore/data/entity/User.java
@@ -2,14 +2,27 @@ package com.belhard.bookstore.data.entity;
 
 import lombok.Data;
 
+import jakarta.persistence.*;
+
 @Data
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "email")
     private String email;
+    @Column(name = "login")
     private String login;
+    @Column(name = "password")
     private String password;
+   @Column(name = "role_id")
     private Role role;
 
     public enum Role {
