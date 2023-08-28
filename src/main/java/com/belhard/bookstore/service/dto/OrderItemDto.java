6195de371db639284dc1eefe3beaf38ad8b86398
IgorChernov <ichernomorr@gmail.com>
2023-08-28 22:13:36 +0300

    Delete field orderId in OrderItemDto

--- src/main/java/com/belhard/bookstore/service/dto/OrderItemDto.java
+++ src/main/java/com/belhard/bookstore/service/dto/OrderItemDto.java
@@ -7,7 +7,6 @@ import java.math.BigDecimal;
 @Data
 public class OrderItemDto {
     private Long id;
-    private Long orderId;
     private BookDto bookDto;
     private Integer bookQuantity;
     private BigDecimal bookPrice;
