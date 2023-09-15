9c91b5acedd02318847ce8f7ec0474c3eeea9e7e
IgorChernov <ichernomorr@gmail.com>
2023-09-15 21:14:28 +0300

    Add ErrorController

--- src/main/java/com/belhard/bookstore/web/ErrorController.java
+++ src/main/java/com/belhard/bookstore/web/ErrorController.java
@@ -1,13 +1,32 @@
 package com.belhard.bookstore.web;
 
+import com.belhard.bookstore.service.exeption.AppException;
+import com.belhard.bookstore.service.exeption.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@ControllerAdvice
+@Log4j2
 public class ErrorController {
-    @GetMapping("/error")
-    public String process(HttpServletRequest req) {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String error(ResourceNotFoundException e, Model model) {
+        model.addAttribute("status", 404);
+        model.addAttribute("message", e.getMessage());
+        log.error(e.getMessage(), e);
+        return "error";
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String error(AppException e, Model model) {
+        model.addAttribute("status", 500);
+        model.addAttribute("message", e.getMessage());
+        log.error(e.getMessage(), e);
         return "error";
     }
 }
