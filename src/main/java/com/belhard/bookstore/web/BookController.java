d7757c229627234e6fb08c1a7ece3ff906c0d9da
IgorChernov <ichernomorr@gmail.com>
2023-09-15 23:35:24 +0300

    Fix BookController

--- src/main/java/com/belhard/bookstore/web/BookController.java
+++ src/main/java/com/belhard/bookstore/web/BookController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 @Log4j2
-@RequestMapping(value = "/books")
+@RequestMapping("/books")
 public class BookController {
     private final BookService bookService;
 
