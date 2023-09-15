6c2692507723de80e10e9f5fce8c8ff6e76aa23e
IgorChernov <ichernomorr@gmail.com>
2023-09-15 23:34:16 +0300

    Fix ErrorController

--- src/main/java/com/belhard/bookstore/web/ErrorController.java
+++ src/main/java/com/belhard/bookstore/web/ErrorController.java
@@ -2,16 +2,24 @@ package com.belhard.bookstore.web;
 
 import com.belhard.bookstore.service.exeption.AppException;
 import com.belhard.bookstore.service.exeption.ResourceNotFoundException;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+@Controller
 @ControllerAdvice
+@RequestMapping("/error")
 @Log4j2
 public class ErrorController {
+
+    @GetMapping
+    public String error(Model model) {
+        model.addAttribute("status", 404);
+        return "error";
+    }
+
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String error(ResourceNotFoundException e, Model model) {
