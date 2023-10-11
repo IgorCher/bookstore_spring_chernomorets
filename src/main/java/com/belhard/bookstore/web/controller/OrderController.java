2e811723a7bf39436b5c7be259e7a03b17ca2fbb
IgorChernov <ichernomorr@gmail.com>
2023-10-11 22:38:10 +0300

    Fix urls in OrderController

--- src/main/java/com/belhard/bookstore/web/controller/OrderController.java
+++ src/main/java/com/belhard/bookstore/web/controller/OrderController.java
@@ -2,39 +2,36 @@ package com.belhard.bookstore.web.controller;
 
 import com.belhard.bookstore.service.OrderService;
 import com.belhard.bookstore.service.dto.OrderDto;
-import com.belhard.bookstore.service.dto.OrderItemDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/orders")
 public class OrderController {
+
     private final OrderService orderService;
 
-    @GetMapping("/{id}")
+    @RequestMapping(value = "/orders/{id}", method = RequestMethod.GET)
     public String getOne(@PathVariable long id, Model model) {
         OrderDto orderDto = orderService.getById(id);
-        List<OrderItemDto> items = orderDto.getOrderItemsDto();
         model.addAttribute("order", orderDto);
         return "order";
     }
 
-    @GetMapping
+    @RequestMapping(value = "/orders", method = RequestMethod.GET)
     public String getAll(Model model) {
         List<OrderDto> orders = orderService.getAll();
         model.addAttribute("orders", orders);
         return "orders";
     }
 
-    @GetMapping("/user_orders/{id}")
+    @RequestMapping(value = "/users/{id}/orders", method = RequestMethod.GET)
     public String getUserOrders(@PathVariable long id, Model model) {
         List<OrderDto> orders = orderService.getByUserId(id);
         model.addAttribute("orders", orders);
