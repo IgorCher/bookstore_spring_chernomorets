a00d6a9fbcd3ceac7d1d29c042a83c5adc7d9d41
IgorChernov <ichernomorr@gmail.com>
2023-10-23 18:49:09 +0300

    Fix ErrorController

--- src/main/java/com/belhard/bookstore/web/controller/ErrorController.java
+++ src/main/java/com/belhard/bookstore/web/controller/ErrorController.java
@@ -2,39 +2,53 @@ package com.belhard.bookstore.web.controller;
 
 import com.belhard.bookstore.service.exeption.AppException;
 import com.belhard.bookstore.service.exeption.ResourceNotFoundException;
+import com.belhard.bookstore.service.exeption.ValidationException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-@Controller
 @ControllerAdvice
-@RequestMapping("/error")
 @Log4j2
 public class ErrorController {
 
-    @GetMapping
-    public String error(Model model) {
-        model.addAttribute("status", 404);
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String processError(RuntimeException e, Model model) {
+        model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        log.error(e.getMessage(), e);
         return "error";
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String error(ResourceNotFoundException e, Model model) {
-        model.addAttribute("status", 404);
+    public String dataError(ResourceNotFoundException e, Model model) {
+        model.addAttribute("status", HttpStatus.NOT_FOUND.value());
         model.addAttribute("message", e.getMessage());
         log.error(e.getMessage(), e);
         return "error";
     }
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String error(AppException e, Model model) {
-        model.addAttribute("status", 500);
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String appError(AppException e, Model model) {
+        model.addAttribute("status", HttpStatus.BAD_REQUEST.value());
         model.addAttribute("message", e.getMessage());
         log.error(e.getMessage(), e);
         return "error";
+    }
+
+    @ExceptionHandler
+    public String validationError(ValidationException e, Model model) {
+        model.addAttribute("message", e.getMessage());
+        return "error";
+    }
+
+    @ExceptionHandler
+    public String loginError(SecurityException e, Model model) {
+        model.addAttribute("message", e.getMessage());
+        return "loginForm";
     }
 }
